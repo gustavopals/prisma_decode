f994e00bda53907e693fc2e80df8169f28faadb1
Gustavo Pals <gustavoandrepals@gmail.com>
2022-05-23 16:55:10 -0300

    Nova tabela Customers

---
+++ prisma/migrations/20220523195051_create_customer/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+  })
+
 app.listen(4003, () => console.log("Server is running on PORT 4003"));
